ec2c5fba3367b7bc72c74c3ce3ecefb220da6883
songmeo <shuong1012@gmail.com>
2020-08-19 11:48:23 +0300

    models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Auction
 
 # Register your models here.
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price")
+admin.site.register(Auction, AuctionAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,27 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.username}"
+
+class Auction(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField(default=0)
+    def __str__(self):
+        return f"{self.name} costs {self.price}"
+
+class Bid(models.Model):
+    price = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bids")
+    
+    def __str__(self):
+        return f"{self.user} bids {self.price} for {self.auction}"
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+    
+    def __str__(self):
+        return f"{self.user} commented: {self.comment} about {self.auction}"
